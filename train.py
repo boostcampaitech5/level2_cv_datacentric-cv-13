557752baf5131ac17943e13d41731d235ed31b08
Junha <jh58power@naver.com>
2023-05-25 18:33:49 +0900

    train 코드 추가

--- train.py
+++ train.py
@@ -1,1 +1,163 @@
 
+import os
+import os.path as osp
+import time
+import math
+from datetime import timedelta
+from argparse import ArgumentParser
+
+import torch
+from torch import cuda
+from torch.utils.data import DataLoader
+from torch.optim import lr_scheduler
+from tqdm import tqdm
+
+from east_dataset import EASTDataset
+from dataset import SceneTextDataset
+from model import EAST
+
+from accelerate import Accelerator
+import numpy as np
+import random
+import wandb
+
+def parse_args():
+    parser = ArgumentParser()
+
+    # Conventional args
+    parser.add_argument('--data_dir', type=str,
+                        default=os.environ.get('SM_CHANNEL_TRAIN', '../data/medical'))
+    parser.add_argument('--model_dir', type=str, default=os.environ.get('SM_MODEL_DIR',
+                                                                        'trained_models'))
+
+    parser.add_argument('--device', default='cuda' if cuda.is_available() else 'cpu')
+    parser.add_argument('--num_workers', type=int, default=8)
+
+    parser.add_argument('--image_size', type=int, default=2048)
+    parser.add_argument('--input_size', type=int, default=1024)
+    parser.add_argument('--batch_size', type=int, default=8)
+    parser.add_argument('--learning_rate', type=float, default=1e-3)
+    parser.add_argument('--max_epoch', type=int, default=150)
+    parser.add_argument('--save_interval', type=int, default=5)
+    parser.add_argument('--ignore_tags', type=list, default=['masked', 'excluded-region', 'maintable', 'stamp'])
+    parser.add_argument('--seed', type=list, default=13)
+    parser.add_argument('--use_wandb', type=list, default=True)
+
+    args = parser.parse_args()
+
+    if args.input_size % 32 != 0:
+        raise ValueError('`input_size` must be a multiple of 32')
+
+    return args
+
+def torch_seed(random_seed):
+    torch.manual_seed(random_seed)
+    torch.cuda.manual_seed(random_seed)
+    torch.cuda.manual_seed_all(random_seed) # if use multi-GPU 
+    # CUDA randomness
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    
+    np.random.seed(random_seed)
+    random.seed(random_seed)
+    os.environ['PYTHONHASHSEED'] = str(random_seed)
+    
+    
+def do_training(data_dir, model_dir, device, image_size, input_size, num_workers, batch_size,
+                learning_rate, max_epoch, save_interval, ignore_tags, seed, use_wandb):
+    
+    torch_seed(seed)
+    
+    if use_wandb:
+        print('Initialize WandB ...')
+        wandb.init(name = f'exp1',
+                   project = "Data-Cnetric",
+                   entity = "connect-cv-13_2")
+    
+    accelerator = Accelerator(
+        gradient_accumulation_steps = 1,
+        mixed_precision             = 'fp16'
+    )
+        
+    dataset = SceneTextDataset(
+        data_dir,
+        split='train',
+        image_size=image_size,
+        crop_size=input_size,
+        ignore_tags=ignore_tags
+    )
+    dataset = EASTDataset(dataset)
+    num_batches = math.ceil(len(dataset) / batch_size)
+    train_loader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers
+    )
+
+    #device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = accelerator.device
+    
+    
+    
+    model = EAST()
+    model.to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[max_epoch // 2], gamma=0.1)
+    
+    model, optimizer, train_loader = accelerator.prepare(model, optimizer, train_loader)
+
+    model.train()
+    for epoch in range(max_epoch):
+        epoch_loss, epoch_start = 0, time.time()
+        with tqdm(total=num_batches) as pbar:
+            for img, gt_score_map, gt_geo_map, roi_mask in train_loader:
+                pbar.set_description('[Epoch {}]'.format(epoch + 1))
+
+                loss, extra_info = model.train_step(img, gt_score_map, gt_geo_map, roi_mask)
+                optimizer.zero_grad()
+
+                # loss.backward()
+                accelerator.backward(loss)
+                optimizer.step()
+
+                loss_val = loss.item()
+                epoch_loss += loss_val
+
+                pbar.update(1)
+                val_dict = {
+                    'Cls loss': extra_info['cls_loss'], 'Angle loss': extra_info['angle_loss'],
+                    'IoU loss': extra_info['iou_loss']
+                }
+                pbar.set_postfix(val_dict)
+                
+                if use_wandb:
+                    wandb.log({'Mean loss': epoch_loss / num_batches,
+                               'Cls loss': extra_info['cls_loss'],
+                               'Angle loss': extra_info['angle_loss'],
+                                'IoU loss': extra_info['iou_loss']
+                            # 'Val Acc': val_acc,
+                            })
+
+        scheduler.step()
+
+        print('Mean loss: {:.4f} | Elapsed time: {}'.format(
+            epoch_loss / num_batches, timedelta(seconds=time.time() - epoch_start)))
+
+        if (epoch + 1) % save_interval == 0:
+            if not osp.exists(model_dir):
+                os.makedirs(model_dir)
+
+            ckpt_fpath = osp.join(model_dir, 'latest.pth')
+            torch.save(model.state_dict(), ckpt_fpath)
+            
+            
+
+
+def main(args):
+    do_training(**args.__dict__)
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
